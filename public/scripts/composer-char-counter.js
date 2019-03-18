223ed90964099579954914417345483a1d256904
Gurp <kooner@gmail.com>
2019-03-18 23:12:28 +0000

    Counter color changes red if more than 140 characters are entered

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
   let char_count = 140;
   $(".new-tweet form textarea").on('keyup', function(event){
     $(".counter").text(char_count - this.value.length);
+    if((char_count - this.value.length) < 0) {
+      $(".counter").css('color', 'red');
+    } else {
+      $(".counter").css('color', 'black');
+    }
   });
 });
