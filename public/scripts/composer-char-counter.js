4ab15e03d1ee9d01d6786d1788c45243dfa6bdde
Gurp <kooner@gmail.com>
2019-03-19 16:10:35 +0000

    refactored char-counter according to mentor code review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   // --- our code goes here ---
   let char_count = 140;
-  $(".new-tweet form textarea").on('keyup', function(event){
-    $(".counter").text(char_count - this.value.length);
+  $(".new-tweet form textarea").on('input', function(event){
+    var counter = $(this).siblings().find('.counter');
+    counter.text(char_count - this.value.length);
     if((char_count - this.value.length) < 0) {
-      $(".counter").css('color', 'red');
+      counter.addClass('over');
     } else {
-      $(".counter").css('color', 'black');
+      counter.removeClass('over');
     }
   });
 });
+//also add css for color of text and use jquery to add/remove the class
